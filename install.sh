bbb57bb676733231396b0f7a1760c6f1048929e3
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-23 21:51:02 +0000

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rm -rf $HOME/.zsh
-rm $HOME/.zshrc && echo "source $HOME/.zsh/zshrc" > $HOME/.zshrc
+rm -rf $HOME/.zsh && rm $HOME/.zshrc
+echo "source $HOME/.zsh/zshrc" > $HOME/.zshrc
 git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
