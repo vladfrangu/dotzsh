4b0d0111c6a7943f0ba12d05a9f30a6121cc0e9e
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-24 18:44:24 +0000

    Fixed no match found error

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-rm -rf $HOME/.zsh*
+touch $HOME/.zsh && touch $HOME/.zcompdump
+rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
 git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
+chsh -s /bin/zsh
