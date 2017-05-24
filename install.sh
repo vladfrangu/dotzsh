b7b8e5aae6fe0d6f34f792daaa3173caaf378a3b
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-24 03:45:11 +0000

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rm -rf $HOME/.zsh && rm $HOME/.zshrc
-echo "source $HOME/.zsh/zshrc" > $HOME/.zshrc
+rm -rf $HOME/.zsh*
+echo "export ZDOTDIR=\$HOME/.zsh\nsource \$ZDOTDIR/.zshenv" > $HOME/.zshenv
 git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
