432059074215f000355521528971bb4199a0cd17
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-24 03:58:17 +0000

    WIP

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ rm -rf $HOME/.zsh*
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
 git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
-zsh
+exec "/bin/zsh"
