6b59fc4160f2f387f731187dca414d752727814e
Arturo Guerra <arturoguerra360@gmail.com>
2017-05-24 14:05:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
 touch $HOME/.zsh && touch $HOME/.zcompdump
 rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
+git clone git@github.com:ArturoGuerra/dotzsh.git $HOME/.zsh
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
 chsh -s /bin/zsh
