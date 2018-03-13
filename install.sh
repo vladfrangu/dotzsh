f4ce3a1e8c84745bea13690615c191c5f34bd46a
Arturo Guerra <arturoguerra360@gmail.com>
2018-03-13 16:56:33 -0600

    Added ngrok

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
 git clone https://github.com/ArturoGuerra/dotzsh.git $HOME/.zsh
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
-[ -d $HOME/bin ] || mkdir $HOME/bin
-cp -r $HOME/.zsh/bin/* $HOME/bin
 chsh -s $(which zsh)
