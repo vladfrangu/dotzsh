bfd27addd716e0a3830db0bfc5c01e4b1fcafb77
Arturo Guerra <arturoguerra360@gmail.com>
2019-09-03 22:39:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
 git clone https://github.com/ArturoGuerra/dotzsh.git $HOME/.zsh
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
-chsh -s $(which zsh)
+echo "Please run zsh and change you shell"
